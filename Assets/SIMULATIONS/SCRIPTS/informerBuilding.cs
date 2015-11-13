bf81bf883e2fbfa399e41312328a84a339fb337a
Dmitry Kuznetzov <megalord2@mail.ru>
2015-11-14 01:41:54 +0300

    Зачатки здания информера

---
+++ Assets/SIMULATIONS/SCRIPTS/informerBuilding.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class informerBuilding : MonoBehaviour {
+
+	public LayerMask PlayerLayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach (Collider goCollider in Physics.OverlapSphere(gameObject.transform.position,4,PlayerLayer)) {
+			GameObject obj = goCollider.gameObject;
+			
+			if(obj.GetComponent<Character>()){
+				Inform (obj);
+			}
+		}
+
+	}
+
+	public void Inform(GameObject villager)
+	{
+
+	}
+}
