0e59e76dccaa7ff2179f1d43b112de9f3474c5f1
Dmitry Kuznetzov <megalord2@mail.ru>
2015-12-05 22:48:05 +0300

    менеджер для управления окнами интерфейса

---
+++ Assets/SIMULATIONS/Interface/Windows/Window.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowManager 
+{
+
+	public static void Open(string windowName, Vector2 location)
+	{
+		GameObject window = Resources.Load ("Windows/" + windowName) as GameObject;
+
+		GameObject newWindow = GameObject.Instantiate (window);
+		newWindow.name.Remove (newWindow.name.Length - "(Clone)".Length);
+
+	}
+
+	public static void Open(string windowName)
+	{
+		Open (windowName, Vector2.zero);
+	}
+	
+
+}
